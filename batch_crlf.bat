faf324561d16afc87e98812a844f78bcabb31ea5
stakiran <stakiran@users.noreply.github.com>
2018-11-14 13:27:34 +0900

    add about gitattributes sample.

---
+++ batch_crlf.bat
@@ -0,0 +1,3 @@
+@echo off
+echo このファイルは絶対に CRLF になる.
+echo .gitattributes で明示的に設定しているため.
---
+++ batch_lf.bat
@@ -0,0 +1,3 @@
+@echo off
+echo このファイルは(LFでつくってコミットしたものだがチェックアウト時は)CRLF になる.
+echo .gitattributes で明示的に設定しているため.
